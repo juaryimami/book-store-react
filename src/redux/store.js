22177ba497be4f8c2c0838fbf67bae4dea043cbe
juharyimami <juharyimami2011@gmail.com>
2022-11-08 15:47:42 +0300

    logger added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+import logger from 'redux-logger';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
   books: booksReducer,
 });
 export default configureStore({
-  reducer,
+  reducer, middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
